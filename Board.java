8c464667f23c4a3d13a6a0a1fc57b5c98666c4ff
kalmanwong <64678874+kalmanwong@users.noreply.github.com>
2024-02-29 19:50:58 -0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public abstract class Board {
-	//hello
+	private ArrayList<ArrayList<Tile>> gameBoard;
+	
+	Board(int height, int width){
+		gameBoard = new ArrayList<>();
+		for(int i; i < height; i++) {
+			//new innerArraylist
+			for(int j; j < width; j++) {
+				//add tile
+			}
+		}
+	}
 }
