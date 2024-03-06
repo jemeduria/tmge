2fd620e1c5c922d5bcc738f9268e0cdb6cb76c19
jemeduria <julianneeduria23@gmail.com>
2024-03-05 23:01:25 -0800

    Doyeon + Jem: finished SameTileMatch logic

--- SameTileMatch.java
+++ SameTileMatch.java
@@ -1,51 +1,38 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SameTileMatch implements Matchable {
 
-    public SameTileMatch() {
-        ;
-    }
+    public SameTileMatch() {}
 
     public List<Tile> match(List<List<Tile>> gameBoard) {
         String toBeMatched = null;
-        for (List<Tile> innerList: gameBoard) {
-            for (Tile tile: innerList) {
+        List<Tile> matched = new ArrayList<>();
+
+        for (List<Tile> innerList : gameBoard) {
+            for (Tile tile : innerList) {
+                // testing display only
                 String display = tile.getDisplay();
-
-                // initialize toBeMatched
-                if (toBeMatched == null) {
-                    if (!(display == null)) {
-                        toBeMatched = tile.getDisplay();
+                
+                // check non-null (via Tile display) and "removed" Memory Tile
+                if (!(display == null) && !(display.equals("X"))) {
+                    // initialize toBeMatched
+                    if (toBeMatched == null) {
+                        toBeMatched = display;
                     }
+                    // find tile with a display that matches toBeMatched
+                    if (tile.getDisplay().equals(toBeMatched)) {
+                        matched.add(tile);
+                    }
+
                 }
-
-                // find tile with a display that matches toBeMatched
-                if ((!(display == null)) && (!(t))) {
-
-                }
-
             }
         }
-            if (toBeMatched == null) {
 
-//        String toBeMatched = null;
-//        for (Tile tile: tiles) {
-//            if (toBeMatched == null) {
-//                // get something to match
-//                 toBeMatched = tile.getDisplay();
-//
-//                 if (toBeMatched == null) {
-//                     return false;
-//                 }
-//            }
-//
-//            // check match for all tiles
-//            if (!tile.getDisplay().equals(toBeMatched)) {
-//                return false;
-//            }
-//        }
-//
-//        // all matched!
-//        return true;
+        if (matched.size() > 1) {
+            return matched;
+        }
+        return null;
     }
+
 }
