dde260350f4584fd68c791558780142dbc24653e
Doyeon-yun <149656922+Doyeon-yun@users.noreply.github.com>
2024-03-12 18:50:54 -0700

    .
    .

--- SimpleDisappear.java
+++ SimpleDisappear.java
@@ -10,16 +10,40 @@ public class SimpleDisappear implements Disappearable {
         // check whether it is vertical or not
         if (tiles.get(0).getRow() == tiles.get(1).getRow()) {
             //vertical
+            if (tiles.get(0).getRow() > tiles.get(3).getRow()) {
+                int row = tiles.get(0).getRow();
+                int column = tiles.get(0).getColumn();
+            }
+            else{
+                int row = tiles.get(3).getRow();
+                int column = tiles.get(3).getColumn();
+            }
+            while (row >= 0 and board[row-1][column] != null){
+                if (row == 0 || boarc[row-1][column] == null){
+                    break;
+                }
+                board[row][column] = board[row-1][column];
+                row -=1;
+            }
+            board[row][column] = null;
         }
-        for (Tile tile : tiles){
-            int x = tile.getRow();
-            int y = tile.getColumn();
-            // remove the board's tile  make tile's display to be null
-//            board[x,y];
-            // swap until it is the top and make the top null or the board is null
-//            for () {
-//                ;
+        else {
+            for (Tile tile : tiles) {
+                int x = tile.getRow();
+                int y = tile.getColumn();
+                while (x >= 0 || board[x-1][y] != null) {
+                    if (x == 0 || board[x-1][y] == null) {
+                        // When y is 0 or the next tile to move is null, break the loop to avoid ArrayIndexOutOfBoundsException
+                        break;
+                    }
+                    board[x][y] = board[x-1][y];
+                    y -= 1;
+                }
+                if (y >= 0) {
+                    board[x][y] = null;
+                }
 //            }
+            }
         }
     }
 
