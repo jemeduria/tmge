d23fe614d1ed5175081fb87d4e3c16cf5d416fe2
jemeduria <julianneeduria23@gmail.com>
2024-03-20 22:08:31 -0700

    smol change

--- HorizontalMatch.java
+++ HorizontalMatch.java
@@ -11,53 +11,34 @@ public class HorizontalMatch implements Matchable {
         int columns = gameBoard.get(0).size();
         List<Tile> matched = new ArrayList<>();
 
-        // for each row
         for (int row = 0; row < rows; row++) {
-            // check matched from the above row
             if (row > 0) {
                 if (matched.size() >= 4) {
-                    // there was a match (of 4+) in the above row
                     return matched; // SUCCESS
                 }
-                // clear all non-4+ matches
                 matched.clear();
             }
 
-            // for each Tile in the row
             for (int col = 0; col < columns; col++) {
-
-                // get display of the Tile
                 Tile currentTile = gameBoard.get(row).get(col);
                 String currentDisplay = currentTile.getDisplay();
 
                 if (currentDisplay != null) {
-                    // if not null: check for matches
-
                     if ((matched.isEmpty()) || (currentDisplay.equals(matched.get(0).getDisplay()))) {
-                        // is the first non-empty Tile found? add
-                        // is a match? add
                         matched.add(currentTile);
-
                     } else if (matched.size() <= 3) {
-                        // not a match AND there are less than 4 Tiles in the matched? reset
                         matched.clear();
                         matched.add(currentTile);
                     }
-
                 } else {
-                    // if null: ignore the tile and/or clear the matches
-
                     if ((!matched.isEmpty()) && (matched.size() <= 3)) {
-                        // there is a null tile between non-null tiles? clear
                         matched.clear();
                     }
-
                 }
             }
         }
 
         if (matched.size() >= 4) {
-            // there was a match (of 4+) in the bottom row
             return matched; // SUCCESS
         }
         return null;
